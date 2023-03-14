9f4103506b4ceb40742dcbf4eeb0212cc1afb4f3
Julian Doherty <madlep@madlep.com>
2023-03-15 01:04:58 +1100

    introduce LookAndSay struct

--- src/days/day10.rs
+++ src/days/day10.rs
@@ -1,3 +1,6 @@
+use std::num::ParseIntError;
+use std::str::FromStr;
+
 pub fn part1(data: &str) -> String {
     run(data, 40).to_string()
 }
@@ -7,13 +10,9 @@ pub fn part2(data: &str) -> String {
 }
 
 fn run(data: &str, n: u32) -> u32 {
-    let nums = collapse(parse(data));
-    let expanded = expand_n(nums, n);
-
-    expanded.iter().map(|cn| cn.count).sum()
+    LookAndSay::from_str(data).unwrap().expand_n(n).len()
 }
 
-const ASCII_TO_INT: u8 = 48u8;
 type Count = u32;
 type N = u8;
 
@@ -43,73 +42,84 @@ impl CountN {
     }
 }
 
-impl ToString for CountN {
-    fn to_string(&self) -> String {
-        format!("{}{}", self.count, self.n)
-    }
-}
-
 enum CountNAppendResult {
     Combined(CountN),
     Boundary(CountN, CountN),
 }
 
-fn parse(s: &str) -> Vec<CountN> {
-    s.bytes()
-        .map(|c| {
-            assert!(c >= ASCII_TO_INT && c <= ASCII_TO_INT + 9);
-            let n = (c - ASCII_TO_INT) as N;
-            CountN::unit(n)
-        })
-        .collect()
+#[derive(Debug, PartialEq)]
+struct LookAndSay {
+    count_ns: Vec<CountN>,
 }
 
-fn collapse(count_ns: Vec<CountN>) -> Vec<CountN> {
-    let (mut combined_count_ns, last_count_n) =
-        count_ns
-            .into_iter()
-            .fold((vec![], None), |(mut acc, prev), count_n| match prev {
-                None => (acc, Some(count_n)),
-                Some(prev_count_n) => match prev_count_n.append(count_n) {
-                    CountNAppendResult::Combined(new_count_n) => (acc, Some(new_count_n)),
-                    CountNAppendResult::Boundary(old_count_n, new_count_n) => {
-                        acc.push(old_count_n);
-                        (acc, Some(new_count_n))
-                    }
-                },
-            });
-    combined_count_ns.push(last_count_n.unwrap());
-    combined_count_ns
-}
-
-fn expand_n(count_ns: Vec<CountN>, n: u32) -> Vec<CountN> {
-    let mut current = count_ns;
-    for _ in 0..n {
-        current = expand(current);
+impl LookAndSay {
+    fn collapse(self) -> Self {
+        let (mut count_ns, last_count_n) =
+            self.count_ns
+                .into_iter()
+                .fold((vec![], None), |(mut acc, prev), count_n| match prev {
+                    None => (acc, Some(count_n)),
+                    Some(prev_count_n) => match prev_count_n.append(count_n) {
+                        CountNAppendResult::Combined(new_count_n) => (acc, Some(new_count_n)),
+                        CountNAppendResult::Boundary(old_count_n, new_count_n) => {
+                            acc.push(old_count_n);
+                            (acc, Some(new_count_n))
+                        }
+                    },
+                });
+        count_ns.push(last_count_n.unwrap());
+        Self { count_ns }
     }
-    current
+
+    fn expand_n(self, n: u32) -> Self {
+        let mut current = self;
+        for _ in 0..n {
+            current = current.expand();
+        }
+        current
+    }
+
+    fn expand(self) -> Self {
+        let count_ns = self
+            .count_ns
+            .iter()
+            .flat_map(|count_n| {
+                let mut count_digits = vec![];
+                let mut current_count = count_n.count;
+                loop {
+                    let digit = current_count % 10;
+                    count_digits.push(CountN::unit(digit.try_into().unwrap()));
+                    current_count = current_count / 10;
+                    if current_count == 0 {
+                        break;
+                    }
+                }
+                count_digits.reverse();
+                count_digits.push(CountN::unit(count_n.n));
+                count_digits
+            })
+            .collect();
+
+        Self { count_ns }.collapse()
+    }
+
+    fn len(&self) -> u32 {
+        self.count_ns.iter().map(|cn| cn.count).sum()
+    }
 }
 
-fn expand(count_ns: Vec<CountN>) -> Vec<CountN> {
-    let expanded = count_ns
-        .iter()
-        .flat_map(|count_n| {
-            let mut count_digits = vec![];
-            let mut current_count = count_n.count;
-            loop {
-                let digit = current_count % 10;
-                count_digits.push(CountN::unit(digit.try_into().unwrap()));
-                current_count = current_count / 10;
-                if current_count == 0 {
-                    break;
-                }
-            }
-            count_digits.reverse();
-            count_digits.push(CountN::unit(count_n.n));
-            count_digits
-        })
-        .collect();
-    collapse(expanded)
+impl FromStr for LookAndSay {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let count_ns = s
+            .chars()
+            .map(|c| c.to_string().parse().and_then(|n| Ok(CountN::unit(n))))
+            .into_iter()
+            .collect::<Result<Vec<CountN>, Self::Err>>()?;
+
+        Ok(Self { count_ns }.collapse())
+    }
 }
 
 #[cfg(test)]
@@ -117,115 +127,183 @@ mod tests {
     use super::*;
 
     #[test]
-    fn parse_test() {
+    fn from_str_test() {
         // 1 becomes 11 (1 copy of digit 1).
-        assert_eq!(parse("1"), vec![CountN::unit(1)]);
+        assert_eq!(
+            LookAndSay::from_str("1").unwrap(),
+            LookAndSay {
+                count_ns: vec![CountN::unit(1)]
+            }
+        );
 
         // 11 becomes 21 (2 copies of digit 1).
-        assert_eq!(parse("11"), vec![CountN::unit(1), CountN::unit(1)]);
+        assert_eq!(
+            LookAndSay::from_str("11").unwrap(),
+            LookAndSay {
+                count_ns: vec![CountN::new(2, 1)]
+            }
+        );
 
         // 21 becomes 1211 (one 2 followed by one 1).
-        assert_eq!(parse("21"), vec![CountN::unit(2), CountN::unit(1)]);
+        assert_eq!(
+            LookAndSay::from_str("21").unwrap(),
+            LookAndSay {
+                count_ns: vec![CountN::unit(2), CountN::unit(1)]
+            }
+        );
 
         // 1211 becomes 111221 (one 1, one 2, and two 1s).
         assert_eq!(
-            parse("1211"),
-            vec![
-                CountN::unit(1),
-                CountN::unit(2),
-                CountN::unit(1),
-                CountN::unit(1)
-            ]
+            LookAndSay::from_str("1211").unwrap(),
+            LookAndSay {
+                count_ns: vec![CountN::unit(1), CountN::unit(2), CountN::new(2, 1),]
+            }
         );
 
         // 111221 becomes 312211 (three 1s, two 2s, and one 1).
         assert_eq!(
-            parse("111221"),
-            vec![
-                CountN::unit(1),
-                CountN::unit(1),
-                CountN::unit(1),
-                CountN::unit(2),
-                CountN::unit(2),
-                CountN::unit(1)
-            ]
+            LookAndSay::from_str("111221").unwrap(),
+            LookAndSay {
+                count_ns: vec![CountN::new(3, 1), CountN::new(2, 2), CountN::unit(1)]
+            }
+        );
+
+        assert_eq!(
+            LookAndSay::from_str("123a456").unwrap_err().to_string(),
+            "invalid digit found in string"
         );
     }
 
     #[test]
     fn collapse_test() {
         // 1 becomes 11 (1 copies of digit 1).
-        assert_eq!(collapse(vec![CountN::unit(1)]), vec![CountN::unit(1)]);
+        assert_eq!(
+            LookAndSay {
+                count_ns: vec![CountN::unit(1)]
+            }
+            .collapse(),
+            LookAndSay {
+                count_ns: vec![CountN::unit(1)]
+            }
+        );
 
         // 11 becomes 21 (2 copies of digit 1).
         assert_eq!(
-            collapse(vec![CountN::unit(1), CountN::unit(1)]),
-            vec![CountN::new(2, 1)]
+            LookAndSay {
+                count_ns: vec![CountN::unit(1), CountN::unit(1)]
+            }
+            .collapse(),
+            LookAndSay {
+                count_ns: vec![CountN::new(2, 1)]
+            }
         );
 
         // 21 becomes 1211 (one 2 followed by one 1).
         assert_eq!(
-            collapse(vec![CountN::unit(2), CountN::unit(1)]),
-            vec![CountN::unit(2), CountN::unit(1)]
+            LookAndSay {
+                count_ns: vec![CountN::unit(2), CountN::unit(1)]
+            }
+            .collapse(),
+            LookAndSay {
+                count_ns: vec![CountN::unit(2), CountN::unit(1)]
+            }
         );
 
         // 1211 becomes 111221 (one 1, one 2, and two 1s).
         assert_eq!(
-            collapse(vec![
-                CountN::unit(1),
-                CountN::unit(2),
-                CountN::unit(1),
-                CountN::unit(1)
-            ]),
-            vec![CountN::unit(1), CountN::unit(2), CountN::new(2, 1)]
+            LookAndSay {
+                count_ns: vec![
+                    CountN::unit(1),
+                    CountN::unit(2),
+                    CountN::unit(1),
+                    CountN::unit(1)
+                ]
+            }
+            .collapse(),
+            LookAndSay {
+                count_ns: vec![CountN::unit(1), CountN::unit(2), CountN::new(2, 1)]
+            }
         );
 
         // 111221 becomes 312211 (three 1s, two 2s, and one 1).
         assert_eq!(
-            collapse(vec![
-                CountN::unit(1),
-                CountN::unit(1),
-                CountN::unit(1),
-                CountN::unit(2),
-                CountN::unit(2),
-                CountN::unit(1)
-            ]),
-            vec![CountN::new(3, 1), CountN::new(2, 2), CountN::unit(1)]
+            LookAndSay {
+                count_ns: vec![
+                    CountN::unit(1),
+                    CountN::unit(1),
+                    CountN::unit(1),
+                    CountN::unit(2),
+                    CountN::unit(2),
+                    CountN::unit(1)
+                ]
+            }
+            .collapse(),
+            LookAndSay {
+                count_ns: vec![CountN::new(3, 1), CountN::new(2, 2), CountN::unit(1)]
+            }
         );
     }
 
     #[test]
     fn expand_test() {
         // 1 becomes 11 (2 copies of digit 1).
-        assert_eq!(expand(vec![CountN::unit(1)]), vec![CountN::new(2, 1)]);
+        assert_eq!(
+            LookAndSay {
+                count_ns: vec![CountN::unit(1)]
+            }
+            .expand(),
+            LookAndSay {
+                count_ns: vec![CountN::new(2, 1)]
+            }
+        );
 
         // 11 becomes 21 (2 copies of digit 1).
         assert_eq!(
-            expand(vec![CountN::new(2, 1)]),
-            vec![CountN::unit(2), CountN::unit(1)]
+            LookAndSay {
+                count_ns: vec![CountN::new(2, 1)]
+            }
+            .expand(),
+            LookAndSay {
+                count_ns: vec![CountN::unit(2), CountN::unit(1)]
+            }
         );
 
         // 21 becomes 1211 (one 2 followed by one 1).
         assert_eq!(
-            expand(vec![CountN::unit(2), CountN::unit(1)]),
-            vec![CountN::unit(1), CountN::unit(2), CountN::new(2, 1)]
+            LookAndSay {
+                count_ns: vec![CountN::unit(2), CountN::unit(1)]
+            }
+            .expand(),
+            LookAndSay {
+                count_ns: vec![CountN::unit(1), CountN::unit(2), CountN::new(2, 1)]
+            }
         );
 
         // 1211 becomes 111221 (one 1, one 2, and two 1s).
         assert_eq!(
-            expand(vec![CountN::unit(1), CountN::unit(2), CountN::new(2, 1)]),
-            vec![CountN::new(3, 1), CountN::new(2, 2), CountN::unit(1)]
+            LookAndSay {
+                count_ns: vec![CountN::unit(1), CountN::unit(2), CountN::new(2, 1)]
+            }
+            .expand(),
+            LookAndSay {
+                count_ns: vec![CountN::new(3, 1), CountN::new(2, 2), CountN::unit(1)]
+            }
         );
 
         // 111221 becomes 312211 (three 1s, two 2s, and one 1).
         assert_eq!(
-            expand(vec![CountN::new(3, 1), CountN::new(2, 2), CountN::unit(1)]),
-            vec![
-                CountN::unit(3),
-                CountN::unit(1),
-                CountN::new(2, 2),
-                CountN::new(2, 1)
-            ]
+            LookAndSay {
+                count_ns: vec![CountN::new(3, 1), CountN::new(2, 2), CountN::unit(1)]
+            }
+            .expand(),
+            LookAndSay {
+                count_ns: vec![
+                    CountN::unit(3),
+                    CountN::unit(1),
+                    CountN::new(2, 2),
+                    CountN::new(2, 1)
+                ]
+            }
         );
     }
 }
